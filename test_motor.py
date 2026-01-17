b3c971e97e1c01033a249f2d2cf8848dbf18148d
EmmardF <emmard.frangopoulos@epfl.ch>
2026-01-17 00:24:32 -0500

    measure distance of a step

--- test_motor.py
+++ test_motor.py
@@ -2,7 +2,8 @@ import pigpio
 import time
 
 STEPX = 17
-DIRX  = 27
+DIRX1  = 27
+DIRX2  = 4
 
 STEPY = 23
 DIRY  = 24
@@ -14,7 +15,8 @@ pi = pigpio.pi()
 assert pi.connected
 
 pi.set_mode(STEPX, pigpio.OUTPUT)
-pi.set_mode(DIRX, pigpio.OUTPUT)
+pi.set_mode(DIRX1, pigpio.OUTPUT)
+pi.set_mode(DIRX2, pigpio.OUTPUT)
 
 pi.set_mode(STEPY, pigpio.OUTPUT)
 pi.set_mode(DIRY, pigpio.OUTPUT)
@@ -22,20 +24,25 @@ pi.set_mode(DIRY, pigpio.OUTPUT)
 pi.set_mode(STEPZ, pigpio.OUTPUT)
 pi.set_mode(DIRZ, pigpio.OUTPUT)
 
+pi.write(DIRX1, 1)
+pi.write(DIRX2, 0)
+pi.write(DIRY, 1)
+pi.write(DIRZ, 1)
+
 pi.write(STEPX, 1)
 pi.write(STEPY, 1)
 pi.write(STEPZ, 1)
 
 time.sleep(1)
 
-for i in range(2000):
+for i in range(425):
     pi.write(STEPX, 1)
     pi.write(STEPY, 1)
     pi.write(STEPZ, 1)
-    time.sleep(0.001)   # 10 ms HIGH
+    time.sleep(0.005)   # 1 ms HIGH
     pi.write(STEPX, 0)
     pi.write(STEPY, 0)
     pi.write(STEPZ, 0)
-    time.sleep(0.001)   # 10 ms LOW
+    time.sleep(0.005)   # 1 ms LOW
 
 pi.stop()
